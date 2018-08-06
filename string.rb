5d14c7c2a2a794eda1e2d214a4d8c56235c73773
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-06 17:40:54 +0000

    automatically backed up by learn

--- string.rb
+++ string.rb
@@ -1,2 +1,8 @@
 puts "jahnavi"
 puts "jahnavi".upcase
+puts "hello".upcase
+puts "Hello".downcase
+puts "Hello".reverse 
+puts "Hello".swapcase 
+puts "Hello".capitalize 
+puts "Greetings People".length
